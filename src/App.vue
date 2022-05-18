27a46f832eaa08085d1724f09784fe7a52c1f943
Abu Zakaria <jakariablaine120@gmail.com>
2022-05-19 03:10:01 +0600

    Add background image

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="overlay"></div>
+    <HomeComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from "./components/HomeComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HomeComponent,
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.wrapper {
+  background: url('assets/background.webp');
+  background-size: cover;
+  height: 100vh;
+}
+
+.overlay {
+  height: 100vh;
+  z-index: 5;
+  background: #000;
+  opacity: 0.4;
 }
 </style>
