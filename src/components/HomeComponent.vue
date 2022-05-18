742ecd16df7b00b6ae3eeeea905a6f2ee4fd60e1
Abu Zakaria <jakariablaine120@gmail.com>
2022-05-19 04:26:11 +0600

    Add some animation to jukebox play button

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,11 +2,34 @@
   <div>
     <div id="home">
       <div class="player">
-        <img src="../assets/jukebox.webp" class="jukebox-img" alt="Play">
+        <button type="button" class="play-btn" :class="{ 'playing': state.isPlaying }" @click="togglePlay">
+          <img src="../assets/jukebox.webp" class="jukebox-img" alt="Play">
+        </button>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      isPlaying: false,
+    });
+
+    const togglePlay = () => {
+      state.isPlaying = !state.isPlaying;
+    }
+
+    return {
+      state,
+      togglePlay,
+    }
+  },
+}
+</script>
 
 <style scoped>
 #home {
@@ -35,6 +58,42 @@
 .jukebox-img {
   width: 40px;
   height: 40px;
+}
+
+.play-btn {
+  background: transparent;
+  border: 0;
+  padding: 15px 18px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.play-btn.playing {
+  animation-name: playingAnim;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-delay: 0s;
+  animation-timing-function: linear;
+}
+
+@keyframes playingAnim {
+  from {
+    box-shadow: 0 0 1px 2px rgba(115, 115, 115, 0.7);
+    transform: rotateZ(0deg);
+  }
+
+  25% {
+    transform: rotateZ(8deg);
+  }
+
+  75% {
+    transform: rotateZ(-8deg);
+  }
+
+  to {
+    box-shadow: 0 0 2px 25px rgba(115, 115, 115, 0);
+    transform: rotateZ(0deg);
+  }
 }
 
 </style>
